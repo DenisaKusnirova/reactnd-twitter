3ad5d524cd832d223c0074932515eb59f499ed31
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-07-05 18:22:16 +0200

    Create NewTweet component

---
+++ src/components/NewTweet.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+class NewTweet extends Component {
+  state = {
+    text: '',
+  }
+
+  handleChange = (e) => {
+    const text = e.target.value
+
+    this.setState(() => ({
+      text
+    }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { text } = this.state
+    // TODO: add a new tweet to the store, redirect to the homepage
+    console.log('New tweet', text)
+
+    this.setState(() => ({
+      text: ''
+    }))
+  }
+
+  render() {
+    const { text } = this.state
+
+    const tweetLeft = 280 - text.length
+
+    return (
+      <div>
+        <h3 className="center">Compose New Tweet</h3>
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="What's happening?"
+            value={text}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && (
+            <div className="tweet-length">
+              {tweetLeft}
+            </div>
+          )}
+          <button
+            type="submit"
+            className="btn"
+            disabled={text === ''}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default NewTweet
